57675e6eebc5a754ffb637f9f2345c2644a2f96c
RamuGoli4 <ramu.goli4@gmail.com>
2025-02-01 22:18:54 -0500

    Initial commit

---
+++ src/main/java/com/spring/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.spring.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class TestController {
+	
+	@GetMapping("test")
+	public String getMethodName() {
+		System.out.println("Testing..");
+		return new String("Hello Spring");
+	}
+	
+
+}
